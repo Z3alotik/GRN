f900704cb9d8c06e0e26965d8a8ff759eb4fdd88
martin.bartosek <martin.bartosek@3pillarglobal.com>
2023-04-28 13:48:13 +0200

    Initial push.

---
+++ src/main/java/com/grove/grn_webapp/GrnWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.grove.grn_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrnWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrnWebAppApplication.class, args);
+    }
+
+}
