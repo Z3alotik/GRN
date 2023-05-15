9f5375b8bbeadacec90173dc15cdfff48ef64100
Z3alotik <11bart11@email.cz>
2023-05-15 12:10:34 +0200

    Enabled H2 DB connection for testing and added User repo and method for adding users.

--- src/main/java/com/grove/grn_webapp/Controllers/UserController.java
+++ src/main/java/com/grove/grn_webapp/Controllers/UserController.java
@@ -1,10 +1,12 @@
 package com.grove.grn_webapp.Controllers;
 
 import com.grove.grn_webapp.Model.User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "api/v1/users")
 public class UserController {
--- src/main/java/com/grove/grn_webapp/GrnWebAppApplication.java
+++ src/main/java/com/grove/grn_webapp/GrnWebAppApplication.java
@@ -1,13 +1,30 @@
 package com.grove.grn_webapp;
 
+import com.grove.grn_webapp.Model.User;
+import com.grove.grn_webapp.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GrnWebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GrnWebAppApplication.class, args);
+
     }
 
+    private void insertUser(UserRepository userRepository) {
+        userRepository.save(new User("Natálie", "Vicherková", "naty.vicherkova@seznam.cz", "111"));
+    }
+
+    @Bean
+    public CommandLineRunner run(UserRepository userRepository) {
+        return (args) -> {
+            insertUser(userRepository);
+            System.out.println(userRepository.findAll());
+        };
+    }
 }
